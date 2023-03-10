5f7016caef456f568102ffd5e0cccd4c8dc31cbb
LeGioNPsyQ <65556998+LeGioNPsyQ@users.noreply.github.com>
2023-03-10 05:35:19 +0100

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,19 @@
+ local checker = pcall(function()
+        games = game:HttpGet("https://raw.githubusercontent.com/LeGioNPsyQ/RBX/main/Games/" .. game.PlaceId .. ".lua")
+    end)
+    if checker then
+        loadstring(games)()
+    else
+        game.StarterGui:SetCore("SendNotification", {
+				Title = "[DevilNetWork X] Notify";
+				Text = "Script is loaded";
+				Duration = 10;
+		})
+		else
+		game.StarterGui:SetCore("SendNotification", {
+				Title = "[DevilNetWork X] Notify";
+				Text = "Game not supported";
+				Duration = 10;
+		})
+    end
+end
