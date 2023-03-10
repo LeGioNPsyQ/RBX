ee5f2e0bb1d924dab87018bcff5b68f55b308105
LeGioNPsyQ <65556998+LeGioNPsyQ@users.noreply.github.com>
2023-03-10 05:40:31 +0100

    Update 8540346411.lua

--- Games/8540346411.lua
+++ Games/8540346411.lua
@@ -1,4 +1,4 @@
-repeat wait() until game:IsLoaded() and game.Players and game.Players.LocalPlayer and game.Players.LocalPlayer.Character
+--repeat wait() until game:IsLoaded() and game.Players and game.Players.LocalPlayer and game.Players.LocalPlayer.Character
 
 --[[
 	###########################################################################################################
