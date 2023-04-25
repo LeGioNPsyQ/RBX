8c37230044ede7052fe20904aebbc80d2097898f
LeGioNPsyQ <65556998+LeGioNPsyQ@users.noreply.github.com>
2023-04-25 23:04:00 +0200

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,20 +1,18 @@
- local checker = pcall(function()
+local checker = pcall(function()
 	GameID = game.PlaceId..".lua"
-        games = game:HttpGet("https://raw.githubusercontent.com/LeGioNPsyQ/RBX/main/Games/"..GameID)
-    end)
-    if checker then
-        loadstring(games)()
-    else
-        game.StarterGui:SetCore("SendNotification", {
-				Title = "[DevilNetWork X] Notify";
-				Text = "Script is loaded";
-				Duration = 10;
-		})
-		else
-		game.StarterGui:SetCore("SendNotification", {
-				Title = "[DevilNetWork X] Notify";
-				Text = "Game not supported";
-				Duration = 10;
-		})
-    end
+	games = game:HttpGet("https://raw.githubusercontent.com/LeGioNPsyQ/RBX/main/Games/"..GameID)
+end)
+if checker then
+	game.StarterGui:SetCore("SendNotification", {
+		Title = "[DevilNetWork X] Notify";
+		Text = "Script is loaded"..games;
+		Duration = 10;
+	})
+	loadstring(games)()
+else
+	game.StarterGui:SetCore("SendNotification", {
+		Title = "[DevilNetWork X] Notify";
+		Text = "Game not supported";
+		Duration = 10;
+	})
 end
